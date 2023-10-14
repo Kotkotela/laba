42e0f11405b72d2ed808fd1cea4502c1fae517c9
Kotkotela <119413756+Kotkotela@users.noreply.github.com>
2023-10-14 16:01:05 +0300

    Add files via upload

---
+++ Entities/RepositoryContext.cs
@@ -0,0 +1,27 @@
+﻿using Entities.Configuration;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Entities
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options)
+        : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CompanyConfiguration());
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+            modelBuilder.ApplyConfiguration(new ayditoryaConfiguration());
+        }
+
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<ayditorya> ayditarya { get; set; }
+        public DbSet<student> student { get; set; }
+    }    
+
+}
