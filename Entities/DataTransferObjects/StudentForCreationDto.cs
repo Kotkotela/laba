4c32a8324c690efdf10a7c30c6b813e2051d7391
Kotkotela <119413756+Kotkotela@users.noreply.github.com>
2023-11-02 01:23:01 +0300

    Add files via upload

---
+++ Entities/DataTransferObjects/StudentForCreationDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities1._0.DataTransferObjects
+{
+    public class StudentForCreationDto : StudentForManipulation
+    {
+    }
+}
